cead1a5a78252fbf4f00a522f63f56e5309e19b5
Func <Funcer@outlook.com>
2025-10-20 15:34:34 +0800

    Use Transliterator from php-intl for pinyin conversion
    Which is a required dependency of MediaWiki core for a long time.

--- includes/Converter.php
+++ includes/Converter.php
@@ -6,22 +6,14 @@ use MediaWiki\Extension\PinyinSort\ConversionTable;
 
 class Converter
 {
-
 	public static function zh2pinyin($string)
 	{
-		$len = mb_strlen($string, 'UTF-8');
-		$builder = '';
-		for ($i = 0; $i < $len; $i++) {
-			$char = mb_substr($string, $i, 1, 'UTF-8');
-			$charLen = strlen($char);
-			if (ord($char[0]) < 128) {
-				$builder .= $char;
-			} else if (isset(ConversionTable::$zh2pinyin[$char])) {
-				$builder .= ucfirst(ConversionTable::$zh2pinyin[$char]);
-			} else {
-				$builder .= '?';
-			}
-		}
-		return $builder;
+		$transliterator = \Transliterator::create('Han-Latin; Latin-ASCII');
+
+		return preg_replace_callback(
+			'/\p{Han}/u',
+			fn($matches) => ucfirst($transliterator->transliterate($matches[0])),
+			$string
+		);
 	}
 }
