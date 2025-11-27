3ad865611e67b32fc946c6b38e4f51960b72112c
WaitSpring <me@waitspring.com>
2025-11-27 19:17:39 +0800

    replace "\" with "use Transliterator" declaration instead

--- includes/Converter.php
+++ includes/Converter.php
@@ -2,11 +2,13 @@
 
 namespace MediaWiki\Extension\PinyinSort;
 
+use Transliterator;
+
 class Converter
 {
 	public static function zh2pinyin($string)
 	{
-		$transliterator = \Transliterator::create('Han-Latin; Latin-ASCII; [:Nonspacing Mark:] Remove; NFC');
+		$transliterator = Transliterator::create('Han-Latin; Latin-ASCII; [:Nonspacing Mark:] Remove; NFC');
 
 		return preg_replace_callback(
 			'/\p{Han}/u',
